124425f531c0dd4839cc9866fe9663aa2fe3fbc3
MANJUNATH <111742470+ManjunathSivasankar@users.noreply.github.com>
2024-02-06 09:44:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ control.addEventListener('click', () => {
 
 // Default timer settings
 let s = 1500;
-timer.textContent = '30:00'
+timer.textContent = '20:00'
 
 // Change timer settings with user input
 radio.addEventListener('change', () => {
